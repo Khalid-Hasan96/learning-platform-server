19463cbdaae7afbbee7b46f6a5fab5b38aabc6a0
Khalid-Hasan96 <khbappy66@gmail.com>
2022-11-01 19:01:04 +0600

    course update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const courses = require('./data/courses.json');
+const allCourses = require('./data/allCourses.json')
 
 app.get('/', (req, res) => {
       res.send('Api Working')
@@ -14,6 +15,15 @@ app.get('/courses', (req, res) => {
       res.send(courses)
 })
 
+app.get('/all-courses', (req, res) => {
+      res.send(allCourses)
+})
+
+app.get('/course/:id', (req, res) => {
+      const id = req.params.id;
+      const providedCourses = allCourses.find(course => course._id === id);
+      res.send(providedCourses)
+})
 
 app.listen(port, () => {
       console.log('Learning Platform Server Runnig on port', port);
